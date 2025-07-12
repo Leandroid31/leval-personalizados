89bd87fa6b5af735c1d4e5593444376f28574bab
Leandroid31 <leandromeadicion@hotmail.com>
2025-07-12 18:06:05 -0300

    Create firebase_options.dart

---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: 'SUA_API_KEY',
+      appId: 'SEU_APP_ID',
+      messagingSenderId: 'SEU_MESSAGING_SENDER_ID',
+      projectId: 'SEU_PROJECT_ID',
+      storageBucket: 'SEU_BUCKET.appspot.com',
+    );
+  }
+}
